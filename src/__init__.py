c2af5134b0f9de399d3f6c3a8c21d954250837eb
Luxraaaaay <751318017@qq.com>
2026-05-11 10:58:27 +0800

    Fix the import error of src

---
+++ src/__init__.py
@@ -0,0 +1,5 @@
+"""Package initializer for the project source code.
+This file makes the `src` directory importable as a package during tests.
+"""
+
+__all__ = ["crawler", "indexer", "search", "main"]
